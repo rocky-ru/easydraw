fc8959f60bc7c029df742a864684aab96b0861f3
rubq <rubingqi@talk-cloud.com>
2018-08-24 17:32:22 +0800

    add entry file

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import canvasRenderer from './core/canvasRenderer'
+import defaultOption from './core/defaultOption'
+import EasyDraw from './core/easyDraw'
+
+export default {
+    canvasRenderer,
+    defaultOption,
+    EasyDraw
+}
